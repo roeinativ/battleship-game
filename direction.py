ccb407b71bba4e2a89efad83d1f6a95372d2fbbb
Roei Nativ <roei.nativ@gmail.com>
2025-05-24 16:27:35 +0300

    deleted files

--- direction.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class Direction(Enum):
-    RIGHT = (0,1)
-    LEFT = (0,-1)
-    UP = (1,0)
-    DOWN = (-1,0)
--- submarine.py
+++
@@ -1,5 +0,0 @@
-class Submarine:
-    def __init__(self,ship_size,ship_position):
-        self.ship_size = ship_size
-        self.ship_position = ship_position
-        
